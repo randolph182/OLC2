705681fb2b0aeefcdfe6569d61e50fb374dd14bf
randolph182 <randolph.estuardo.2012@gmail.com>
2019-03-07 15:54:58 -0600

    instruccion If ok

--- OLC2_Proyecto1/src/FuncionScript/AST/Expresiones/Operacion/Logica.java
+++ OLC2_Proyecto1/src/FuncionScript/AST/Expresiones/Operacion/Logica.java
@@ -14,6 +14,7 @@ import FuncionScript.Entorno.Tipo;
  * @author rm
  */
 public class Logica extends Operacion implements Expresion{
+    
 
     public Logica(Expresion exp1, Expresion exp2, Operador tipoOperador, int linea) {
         super(exp1, exp2, tipoOperador, linea);
--- OLC2_Proyecto1/src/FuncionScript/AST/Instrucciones/Condicionales/If.java
+++ OLC2_Proyecto1/src/FuncionScript/AST/Instrucciones/Condicionales/If.java
@@ -6,9 +6,13 @@
 package FuncionScript.AST.Instrucciones.Condicionales;
 
 import FuncionScript.AST.Expresiones.Expresion;
+import FuncionScript.AST.Expresiones.Identificador;
+import FuncionScript.AST.Expresiones.Operacion.Unario;
 import FuncionScript.AST.Instrucciones.Instruccion;
 import FuncionScript.AST.nodoAST;
 import FuncionScript.Entorno.Entorno;
+import FuncionScript.Entorno.Simbolo;
+import FuncionScript.Entorno.Tipo;
 import java.util.LinkedList;
 
 /**
@@ -19,19 +23,90 @@ public class If  implements Instruccion{
 
 
     Expresion condicion;
-    LinkedList<nodoAST> sentencias;
+    LinkedList<nodoAST> sentencias1;
+    LinkedList<nodoAST> sentencias2;
+    If elseIf = null;
+    boolean hay_else = false;
     int linea;
     
     public If(Expresion condicion, LinkedList<nodoAST> sentencias, int linea) {
         this.condicion = condicion;
-        this.sentencias = sentencias;
+        this.sentencias1 = sentencias;
         this.linea = linea;
+        this.hay_else = false;
     }
+    
+    public If(Expresion cond, LinkedList<nodoAST> s1, LinkedList<nodoAST> s2, int linea) {
+        this.condicion = cond;
+        this.sentencias1 = s1;
+        this.sentencias2 = s2;
+        this.linea = linea;
+        this.hay_else = true;
+    }
+    
+    public If(Expresion cond, LinkedList<nodoAST> ins,If tIf, int linea) {
+        this.condicion = cond;
+        this.sentencias1 = ins;
+        this.elseIf = tIf;
+        this.linea = linea;
+        this.hay_else = false;
+    }
+    
     
     @Override
     public Object ejecutar(Entorno ent) {
+        Object a = null;
+        Tipo tipoA = null;
+         boolean flag = false;
+        if(condicion instanceof Unario){
+             a = (condicion == null) ? null : condicion.getValor(ent);
+             tipoA = condicion.getTipo(ent);
+            if(tipoA.isBoolean()){
+                if (a instanceof String){
+                    if(a.toString().equals("verdadero"))
+                        flag = true;
+                    else 
+                        flag = false;
+                }
+            } else{
+                System.out.println("Error en el retorno de getValor Logica");
+            }
+        } else if(condicion instanceof Identificador){
+            a = ((Identificador)condicion).getIdentificador();
+                if(ent.get((String)a) != null){
+                       Simbolo s = ent.get((String)a);
+                       tipoA = s.getTipo();
+                       if(tipoA.isBoolean()){
+                           if(s.getValor().toString().equals("verdadero"))
+                               flag = true;
+                           else
+                               flag = false;
+                       }
+                } else{
+                    System.out.println("Error no se encontro identificador en UF");
+                    flag = false;
+                }       
+        } else{
+            flag = (boolean)condicion.getValor(ent);
+        }
         Entorno nuevoEnt = new Entorno(ent); //apuntamos al anterior
-        
+        if(flag){
+            for(nodoAST nodo: sentencias1){
+                if(nodo instanceof Instruccion){
+                    Instruccion instruccion = (Instruccion)nodo;
+                    instruccion.ejecutar(nuevoEnt);
+                }
+            }
+        } else if(elseIf != null){ 
+            elseIf.ejecutar(ent);
+        }else if(hay_else){
+            for(nodoAST nodo: sentencias2){
+                if(nodo instanceof Instruccion){
+                    Instruccion instruccion = (Instruccion)nodo;
+                    instruccion.ejecutar(nuevoEnt);
+                }
+            }
+        }
         return null;
     }
 
